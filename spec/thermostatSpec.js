df2cf85675be831b4107242c30a386b89d363012
pyan83 <puyanwei@gmail.com>
2017-08-30 21:14:26 +0100

    added temp up and down, resetting and toggling powermodes

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -10,4 +10,57 @@ describe('Thermostat', function() {
       expect(thermostat.temp).toEqual(20);
     })
   })
+
+  describe("#initialize", function() {
+    it("power saving mode is on by default", function() {
+      expect(thermostat.powerMode).toBe(true);
+    })
+  })
+
+  describe("#up", function() {
+    it("makes the temperature go up by 10", function() {
+      thermostat.up();
+      expect(thermostat.temp).toEqual(30);
+    })
+  })
+
+  describe("#down", function() {
+    it("makes the temperature go down by 10", function() {
+      thermostat.down();
+      expect(thermostat.temp).toEqual(10);
+    })
+  })
+
+  describe("#down", function() {
+    it("cannot go lower then 10", function() {
+      thermostat.temp = 10;
+      thermostat.down()
+      expect(function() {
+        thermostat.down()
+      }).toThrow("temperature cannot be lower then 10");
+    })
+  })
+
+  describe('#reset', function() {
+    it("resets temperature to 20", function() {
+      thermostat.temp = 50;
+      thermostat.reset();
+      expect(thermostat.temp).toEqual(20);
+    })
+  })
+
+  describe('#toggleMode', function() {
+    it("toggles the mode on and off", function() {
+      thermostat.toggleMode();
+      expect(thermostat.powerMode).toBe(false);
+    })
+  })
+
+  // describe('#up', function() {
+  //   it("cannot go higher then 32", function() {
+  //     this.powerMode = false;
+  //     expect(thermostat.up)
+  //   })
+  // })
+
 })
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,3 +1,23 @@
 var Thermostat = function() {
-  this.temp = 20
+  this.temp = 20;
+  this.powerMode = true
 }
+
+Thermostat.prototype.up = function() {
+  this.temp += 10;
+}
+
+Thermostat.prototype.down = function() {
+  if (this.temp < 10) {
+    throw ("temperature cannot be lower then 10");
+  }
+  this.temp -= 10;
+}
+
+Thermostat.prototype.reset = function() {
+  this.temp = 20;
+};
+
+Thermostat.prototype.toggleMode = function() {
+  this.powerMode = !this.powerMode;
+};

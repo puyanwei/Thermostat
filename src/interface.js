9c6c78a9fa7d1d54d6332dd0b152cfd83e53dea1
pyan83 <puyanwei@gmail.com>
2017-11-18 15:11:56 +0800

    seperating city and thermostat temperature id, improving formatting

--- src/interface.js
+++ src/interface.js
@@ -39,14 +39,14 @@ $(document).ready(() => {
     var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city;
     var token = '&appid=72a46f472cd78125c67ce1ba75151860';
     var units = '&units=metric';
-    $.get(url + token + units, function(data) {
-      $('#current-temperature').text(Math.round(data.main.temp));
-      $('#city').text(city);
+    $.get(url + token + units, data => {
+      $('#city-temperature').text(Math.round(data.main.temp));
+      $('#city').text(data.name);
     });
   }
 
   function updateTemperature() {
-    $('#temperature').text(thermostat.temp);
+    $('#manual-temperature').text(thermostat.temp);
     $('#background').attr('class', thermostat.printUsage());
   }
 });

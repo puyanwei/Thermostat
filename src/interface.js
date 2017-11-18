ae741ef9376a911df58488f0c21bf3d17d8b283e
pyan83 <puyanwei@gmail.com>
2017-11-18 14:24:19 +0800

    added user input for city and api call for temperature

--- src/interface.js
+++ src/interface.js
@@ -27,29 +27,23 @@ $(document).ready(() => {
     $('#power-saving-status').text('off');
   });
 
-  $('#current-city').change(function() {
+  displayWeather('London');
+
+  $('#select-city').submit(event => {
+    event.preventDefault();
     var city = $('#current-city').val();
-    $.get(
-      'http://api.openweathermap.org/data/2.5/weather?q=' +
-        city +
-        '&appid=72a46f472cd78125c67ce1ba75151860&units=metric',
-      function(data) {
-        $('#current-temperature').text(data.main.temp);
-      }
-    );
-    console.log(city);
-    // 72a46f472cd78125c67ce1ba75151860
+    displayWeather(city);
   });
 
-  // function updateWeather(city) {
-  //   var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city;
-  //   var token = '&appid=1165259d6e26ca0f8e03387e1f2da5ac';
-  //   var units = '&units=metric';
-  //   $.get(url + token + units, function(response) {
-  //     $('#current-temp').text(Math.round(response.main.temp));
-  //     $('#city').text(response.name);
-  //   });
-  // }
+  function displayWeather(city) {
+    var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city;
+    var token = '&appid=72a46f472cd78125c67ce1ba75151860';
+    var units = '&units=metric';
+    $.get(url + token + units, function(data) {
+      $('#current-temperature').text(data.main.temp);
+      $('#city').text(city);
+    });
+  }
 
   function updateTemperature() {
     $('#temperature').text(thermostat.temp);

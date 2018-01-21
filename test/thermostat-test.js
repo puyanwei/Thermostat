3d166acb7146b6433a2f22dd9c91591a877d7ccb
Puyan Wei <puyanwei@gmail.com>
2018-01-21 23:05:41 +0000

    moved screenshot to top

--- test/thermostat-test.js
+++ test/thermostat-test.js
@@ -1,117 +1,119 @@
-describe('Thermostat', () => {
+describe("Thermostat", () => {
   var thermostat;
 
-  beforeEach(() => (thermostat = new Thermostat()));
+  beforeEach(() => {
+    thermostat = new Thermostat();
+  });
 
-  describe('#initialize', () => {
-    it('starting temperature is 20 degrees', () => {
+  describe("#initialize", () => {
+    it("starting temperature is 20 degrees", () => {
       expect(thermostat.temp).toEqual(20);
     });
 
-    it('power saving mode is on by default', () => {
+    it("power saving mode is on by default", () => {
       expect(thermostat.powerMode).toBe(true);
     });
   });
 
-  describe('#up', () => {
-    it('makes the temperature go up by 1 degrees', () => {
+  describe("#up", () => {
+    it("makes the temperature go up by 1 degrees", () => {
       thermostat.up();
       expect(thermostat.temp).toEqual(21);
     });
 
-    it('cannot go higher then 25 when powermode is on', () => {
+    it("cannot go higher then 25 when powermode is on", () => {
       thermostat.temp = 25;
       expect(() => {
         thermostat.up();
-      }).toThrow('temperature cannot be higher then 25 degrees');
+      }).toThrow("temperature cannot be higher then 25 degrees");
     });
 
-    it('cannot go higher then 32 when powermode is off', () => {
+    it("cannot go higher then 32 when powermode is off", () => {
       thermostat.temp = 32;
       thermostat.off();
       expect(() => {
         thermostat.up();
-      }).toThrow('temperature cannot be higher then 32 degrees');
+      }).toThrow("temperature cannot be higher then 32 degrees");
     });
   });
 
-  describe('#down', () => {
-    it('makes the temperature go down by 1 degrees', () => {
+  describe("#down", () => {
+    it("makes the temperature go down by 1 degrees", () => {
       thermostat.down();
       expect(thermostat.temp).toEqual(19);
     });
 
-    it('cannot go lower then 10', () => {
+    it("cannot go lower then 10", () => {
       thermostat.temp = 11;
       thermostat.down();
       expect(() => {
         thermostat.down();
-      }).toThrow('temperature cannot be lower then 10 degrees');
+      }).toThrow("temperature cannot be lower then 10 degrees");
     });
   });
 
-  describe('#reset', () => {
-    it('resets temperature to 20 degrees', () => {
+  describe("#reset", () => {
+    it("resets temperature to 20 degrees", () => {
       thermostat.temp = 50;
       thermostat.reset();
       expect(thermostat.temp).toEqual(20);
     });
   });
 
-  describe('#on', () => {
-    it('toggles the mode on', () => {
+  describe("#on", () => {
+    it("toggles the mode on", () => {
       thermostat.powerMode = false;
       thermostat.on();
       expect(thermostat.powerMode).toBe(true);
     });
   });
 
-  describe('#on', () => {
-    it('toggles the mode on', () => {
+  describe("#on", () => {
+    it("toggles the mode on", () => {
       thermostat.powerMode = false;
       thermostat.on();
       expect(thermostat.powerMode).toBe(true);
     });
   });
 
-  describe('#powerModeOn', () => {
-    it('is turned on', () => {
+  describe("#powerModeOn", () => {
+    it("is turned on", () => {
       thermostat.powerMode = false;
       thermostat.powerModeOn();
       expect(thermostat.powerMode).toEqual(true);
     });
   });
 
-  describe('#powerModeOff', () => {
-    it('is turned off', () => {
+  describe("#powerModeOff", () => {
+    it("is turned off", () => {
       thermostat.powerModeOff();
       expect(thermostat.powerMode).toEqual(false);
     });
   });
 
-  describe('#lowUsage', () => {
-    it('displays the current usage, which is low', () => {
+  describe("#lowUsage", () => {
+    it("displays the current usage, which is low", () => {
       thermostat.temp = 15;
-      expect(thermostat.printUsage()).toEqual('low-usage');
+      expect(thermostat.printUsage()).toEqual("low-usage");
     });
   });
 
-  describe('#mediumUsage', () => {
-    it('displays the current usage, which is medium', () => {
+  describe("#mediumUsage", () => {
+    it("displays the current usage, which is medium", () => {
       thermostat.temp = 20;
-      expect(thermostat.printUsage()).toEqual('medium-usage');
+      expect(thermostat.printUsage()).toEqual("medium-usage");
     });
   });
 
-  describe('#highUsage', () => {
-    it('displays the current usage, which is high', () => {
+  describe("#highUsage", () => {
+    it("displays the current usage, which is high", () => {
       thermostat.temp = 27;
-      expect(thermostat.printUsage()).toEqual('high-usage');
+      expect(thermostat.printUsage()).toEqual("high-usage");
     });
   });
 
-  describe('#showTemp', () => {
-    it('displays the current temperature', () => {
+  describe("#showTemp", () => {
+    it("displays the current temperature", () => {
       thermostat.temp = 32;
       expect(thermostat.showTemp()).toEqual(32);
     });
